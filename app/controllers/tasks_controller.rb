89ec119af75ea979e1df5a7aa9e8461513e7a71b
adrian_skowron <adrian_skowron@MacBook-Pro-adrian-skowron.local>
2023-09-28 22:20:02 +0200

    Improvements for tasks in tasks_controller and index view.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,11 +2,15 @@ class TasksController < ApplicationController
   before_action :set_task, only: [:destroy]
 
   def index
-    tasks_with_deadline = Task.where.not(finish_date: nil).order(finish_date: :asc)
+    tasks_with_future_deadline = Task.where("finish_date >= ?", Date.today).order(finish_date: :desc)
+    tasks_with_past_deadline = Task.where("finish_date < ?", Date.today).order(finish_date: :asc)
+    #tasks_with_deadline = Task.where.not(finish_date: nil).order(finish_date: :asc)
     tasks_without_deadline = Task.where(finish_date: nil).order(created_at: :asc)
-    @tasks = tasks_with_deadline + tasks_without_deadline
+    @tasks = tasks_with_future_deadline + tasks_with_past_deadline + tasks_without_deadline
+    @incomplete_tasks = @tasks.reject(&:completed?)
+    #@tasks = tasks_with_deadline + tasks_without_deadline
     #@tasks = Task.order(finish_date: :asc)
-    @task_number = 1
+    #@task_number = 1
   end
 
   def new
@@ -28,7 +32,15 @@ class TasksController < ApplicationController
     redirect_to tasks_url, notice: 'Task successfully destroyed.'
   end
 
-  private
+=begin
+  def finish
+    @task = Task.find(params[:id])
+    @task.update(completed: true)
+    redirect_to tasks_url, notice: 'Task successfully finished.'
+  end 
+=end
+
+private
 
   def set_task
     @task = Task.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root to: "tasks#index"
-  resources :tasks, only: [:index, :new, :create, :destroy]
+  resources :tasks, only: [:index, :new, :create, :destroy] do
+    member do
+      put :finish
+    end
+  end
 end
